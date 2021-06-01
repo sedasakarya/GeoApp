d2813b81915163f07b538b23a51aa96476a9e76a
sedasakarya <sedasakarya77@gmail.com>
2021-06-01 09:03:49 +0200

    AccountList component

--- force-app/main/default/aura/AccountSearch/AccountSearchHelper.js
+++ force-app/main/default/aura/AccountSearch/AccountSearchHelper.js
@@ -1,5 +1,18 @@
 ({
-    helperMethod : function() {
-
+    // code in the helper is reusable by both
+    // the controller.js and helper.js files
+    handleSearch: function( component, searchTerm ) {
+        var action = component.get( "c.searchAccounts" );
+        action.setParams({
+            searchTerm: searchTerm
+        });
+        action.setCallback( this, function( response ) {
+            var event = $A.get( "e.c:AccountsLoaded" );
+            event.setParams({
+                "accounts": response.getReturnValue()
+            });
+            event.fire();
+        });
+        $A.enqueueAction( action );
     }
 })
